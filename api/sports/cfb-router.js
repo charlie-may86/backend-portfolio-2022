28f0fc0541fcbc3e64c18d38470d5ba1c40109f4
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2022-07-19 08:45:31 -0500

    hit the schedule

--- api/sports/cfb-router.js
+++ api/sports/cfb-router.js
@@ -1,8 +1,29 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const axios = require("axios");
 
+router.get("/", async (req, res, next) => {
+  const options = {
+    method: "GET",
+    url: "https://api.sportsdata.io/v3/cfb/scores/json/Games/2022",
+    headers: {
+      "Ocp-Apim-Subscription-Key": "b78d05aaa3da480c8f022c79d284bb7d",
+    },
+  };
 
-router.get('/', async (req, res, next) => {
-    res.send("<h1>Hey There good Looking<h1>");
-})
+  const getSchedule = async () => {
+    try {
+      return await axios(options);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-module.exports = router
+  try {
+    const schedule = await getSchedule();
+    res.status(201).json(schedule.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
